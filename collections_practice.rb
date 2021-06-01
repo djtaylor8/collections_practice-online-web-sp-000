5ca8801b0b9c5b53be1470d43e49165ed90172b6
DJ Taylor <dtaylor88@gmail.com>
2021-06-01 04:36:15 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -20,3 +20,20 @@ def swap_elements(array)
   array[1], array[2] = array[2], array[1]
   array
 end
+
+def reverse_array(array)
+  
+end
+
+def kesha_maker(array)
+end
+
+def find_a(array)
+end
+
+def sum_array(array)
+end
+
+def add_s(array)
+end
+
